a2022b4de540e8793119b1f6ad426bb245adeb67
yosukeendo <yosuke.endo@thedott.io>
2023-07-03 19:53:20 +0900

    クイズ画面に問題、写真・動画、選択肢を表示できるようにしました。 説明画面からクイズ画面に遷移できるようにしました。

---
+++ angular-app/src/app/dataContent.ts
@@ -0,0 +1,5 @@
+import { ImageData } from "./imgData";
+
+export const DATA: ImageData[] = [
+  {quizText: '後ろに見える山は何でしょう?', quizImg: 'https://drive.google.com/file/d/14ZKtwNlNXahxMutSJW_paBAxvvoim0qw/preview', ansOptions: ["岩手山", "蔵王山", "大雪山", "安達太良山"], ansWord: '岩手山は日本百名山に選定されており、標高2,038mの成層火山です。冬になれば綺麗な雪山を見ることができます。', ansImg: '"https://drive.google.com/file/d/14ZKtwNlNXahxMutSJW_paBAxvvoim0qw/preview"'},
+]
