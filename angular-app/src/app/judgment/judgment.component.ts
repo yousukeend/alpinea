369f8428c9002ba2e7d5b48a5623ae09990bce8c
yosukeendo <yosuke.endo@thedott.io>
2023-07-17 21:30:07 +0900

    判定画面でクイズの正解のデータを表示させるように変更しました。

--- angular-app/src/app/judgment/judgment.component.ts
+++ angular-app/src/app/judgment/judgment.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { QUIZ_LIST } from '../const';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-judgment',
   templateUrl: './judgment.component.html',
-  styleUrls: ['./judgment.component.scss']
+  styleUrls: ['./judgment.component.scss'],
 })
 export class JudgmentComponent {
+  quizList = QUIZ_LIST;
+  trustUrl: SafeResourceUrl;
 
+  constructor(private sanitizer: DomSanitizer) {
+    this.trustUrl = sanitizer.bypassSecurityTrustResourceUrl(
+      this.quizList[1].ansImg
+    );
+  }
 }
